376d4356bb18f7039277b5c049cba254f0890a94
mandadi-srija <mandadisrija324@gmail.com>
2024-11-20 23:45:15 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int num=4;
+        int ans=1;
+        for(int i=1;i<=num;i++)
+        {
+            ans=ans*i;
+        }
+        System.out.println(ans);
+    }
+}
